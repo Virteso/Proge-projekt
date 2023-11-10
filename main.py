019625fbb2bb754a046accd118c717d4e0f08004
Virteso <rasmus.lille0@gmail.com>
2023-11-10 11:50:00 +0200

    Testing
    Testing some starting code

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pygame as pg
+from sys import exit
 
+
+# Setup
 pg.init()
 screen = pg.display.set_mode((800, 400))
+pg.display.set_caption('nimi')
+clock = pg.time.Clock()
+
+
+
+
+# Aken
+while 1:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+    
+
+    
+    pg.display.update()
+    clock.tick(60)
