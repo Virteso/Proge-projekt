a67ce7de89392cbf57cf46041b92732d0b04e830
langjans <146109682+langjans@users.noreply.github.com>
2023-12-10 19:45:15 +0200

    start/game/game over screen

--- testing.py
+++ testing.py
@@ -10,6 +10,7 @@ screen = pg.display.set_mode((16 * suurus, 9 * suurus))
 pg.display.set_caption('Actual game')
 clock = pg.time.Clock()
 georgia = pg.font.Font('src/fonts/georgia.ttf', 50)
+game_state = "start_menu" 
 
 # Variables
 fps = 60
@@ -31,6 +32,26 @@ player_box = player.get_rect(midbottom = (200, 600))
 red_box = ctest.get_rect(bottomleft = (random.randint(0, 1620), random.randint(0, 1080)))
 text_box = text_surface.get_rect(topleft = (50, 50))
 
+#state
+
+def draw_start_menu():
+   screen.fill((0, 0, 0))
+   font = pg.font.SysFont(None, 40)
+   title = font.render('Stickman', True, (255, 255, 255))
+   start_button = font.render('Press *Space* to Start', True, (255, 255, 255))
+   screen.blit(title, ((16*suurus)/2 - title.get_width()/2, (9*suurus)/2 - title.get_height()/2))
+   screen.blit(start_button, ((16*suurus)/2 - start_button.get_width()/2, (9*suurus)/2 + start_button.get_height()/2))
+   pg.display.update()
+
+def draw_game_over():
+    screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
+    font = pg.font.SysFont(None, 40)
+    title = font.render('Game over', True, (255, 0, 0))
+    restart_button = font.render('R to restart', True, (255, 255, 255))
+    screen.blit(title, ((16*suurus)/2 - title.get_width()/2, (9*suurus)/2 - title.get_height()/2))
+    screen.blit(restart_button, ((16*suurus)/2 - restart_button.get_width()/2, (9*suurus)/2 + restart_button.get_height()/2))
+    pg.display.update()
 
 # Window animation
 while 1:
@@ -39,47 +60,61 @@ while 1:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-    
-    # Key presses
-    # keys = pg.key.get_pressed()
-    # space = keys[pg.K_SPACE]
+        if game_state == "start_menu":
+           draw_start_menu()
+           keys = pg.key.get_pressed()
+           if keys[pg.K_SPACE]:
+               game_state = "game"
+               
+        if game_state == "game_over":
+            draw_game_over()
+            keys = pg.key.get_pressed()
+            if keys[pg.K_r]:
+               game_state = "game"
+           
+    if game_state == "game":
+        
+            
+        # Key presses
+        # keys = pg.key.get_pressed()
+        # space = keys[pg.K_SPACE]
 
-    '''event.type == pg.KEYDOWN
-    event.type == pg.KEYUP
-    event.key == pg.K_SPACE'''
+        '''event.type == pg.KEYDOWN
+        event.type == pg.KEYUP
+        event.key == pg.K_SPACE'''
 
 
-    # Drawing surfaces on the screen
-    screen.blit(background,(bg_box))
-    screen.blit(background2,(bg_box2))
-    screen.blit(player,(player_box))
-    screen.blit(ctest, (red_box))
-    screen.blit(text_surface, (text_box))
-    
-    # Mowing loop
-    if player_box.right == 1800:
-        player_box.left = 120
-        bg_box.left -= 1680
-        bg_box2.left -= 1680
-        if bg_box.right < 0:
-            bg_box.left += 3840
-        if bg_box2.right < 0:
-            bg_box2.left += 3840
-    player_box.right += 5
+        # Drawing surfaces on the screen
+        screen.blit(background,(bg_box))
+        screen.blit(background2,(bg_box2))
+        screen.blit(player,(player_box))
+        screen.blit(ctest, (red_box))
+        screen.blit(text_surface, (text_box))
+        
+        # Mowing loop
+        if player_box.right == 1800:
+            player_box.left = 120
+            bg_box.left -= 1680
+            bg_box2.left -= 1680
+            if bg_box.right < 0:
+                bg_box.left += 3840
+            if bg_box2.right < 0:
+                bg_box2.left += 3840
+        player_box.right += 5
 
-    # Collision and click test
-    mouse_pos = pg.mouse.get_pos()
-    if pg.mouse.get_pressed()[0]:
-        if red_box.collidepoint(mouse_pos):
-            red_box.right = (random.randint(300, 1920))
-            red_box.top = (random.randint(0, 780))
-            score += 1
-            text_surface = georgia.render(f'Score: {score}', True, 'Black')
+        # Collision and click test
+        mouse_pos = pg.mouse.get_pos()
+        if pg.mouse.get_pressed()[0]:
+            if red_box.collidepoint(mouse_pos):
+                red_box.right = (random.randint(300, 1920))
+                red_box.top = (random.randint(0, 780))
+                score += 1
+                text_surface = georgia.render(f'Score: {score}', True, 'Black')
 
-    # Quiting the game
-    if score == 10:
-        pg.quit()
-        exit()
+        # Quiting the game
+        if score == 10:
+            pg.quit()
+            exit()
 
     
     
